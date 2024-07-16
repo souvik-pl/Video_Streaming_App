0e9c2873eda3cb219f490631924025f6ae90ab16
souvik-pl <souvik.9401@gmail.com>
2024-07-16 13:58:38 +0530

    feat: shadcn installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./components/ui/button";
+import { Switch } from "./components/ui/switch";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="dark">
+      <Button>Click here</Button>
+      <Switch />
+    </div>
+  );
 }
 
 export default App;
